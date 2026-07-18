e4a4c84c2f7134d3bd99e393cf15c8f545c02b61
Haris Lania <haris.lania@gmail.com>
2026-07-18 16:51:33 +0400

    chore: update production url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 // Development environment, used by `ng serve` / `ng build -c development`.
 export const environment = {
   production: false,
-  configUrl: '/config.json',
+  configUrl: '/config-development.json',
 };
